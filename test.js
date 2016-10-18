61f6c1f047720b6eeff0f93d011c2ce8fbe0b1db
Chris Gaona <thebeloved88@sbcglobal.net>
2016-10-18 08:32:04 -0700

    Adds public static middeware; tests for correct content type on '/' route

--- test.js
+++ test.js
@@ -3,15 +3,23 @@ var app = require('./app');
 
 
 describe('Requests to the root path', function () {
-   it('should return a 200 status code', function (done) {
+    it('should return a 200 status code', function (done) {
        request(app)
            .get('/')
-           .expect(200)
-           .end(function (err) {
-               if (err) throw error;
-               done();
-           });
-   });
+           .expect(200, done);
+    });
+
+    it('should return a HTML format', function (done) {
+        request(app)
+            .get('/')
+            .expect('Content-Type', /html/, done);
+    });
+
+    it('should return index file with cities', function (done) {
+       request(app)
+           .get('/')
+           .expect(/cities/i, done);
+    });
 });
 
 describe('Listing cities on /cities using GET ', function () {
