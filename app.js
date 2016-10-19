8f65b671cb81e682265466ab59ae78d7f4fe905a
Chris Gaona <thebeloved88@sbcglobal.net>
2016-10-18 20:39:29 -0700

    Fixes gitignore file

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + '/public'));
-
-var cities = require('./routes/cities');
-app.use('/cities', cities);
-
-module.exports = app;
