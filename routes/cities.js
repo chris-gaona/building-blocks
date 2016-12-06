fb00f8c88445f46e9ecf0ea1ec66922d6beb913a
Chris Gaona <thebeloved88@sbcglobal.net>
2016-12-05 19:09:24 -0800

    Adds redistogo to prod instance on Heroku

--- routes/cities.js
+++ routes/cities.js
@@ -2,10 +2,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var urlencode = bodyParser.urlencoded({ extended: false });
 
+// Redis connection
 var redis = require('redis');
-var client = redis.createClient();
 
-client.select((process.env.NODE_ENV || 'development').length);
+if (process.env.REDISTOGO_URL) {
+    var rtg = require("url").parse(process.env.REDISTOGO_URL);
+    var client = redis.createClient(rtg.port, rtg.hostname);
+    client.auth(rtg.auth.split(":")[1]);
+} else {
+    var client = redis.createClient();
+    client.select((process.env.NODE_ENV || 'development').length);
+}
+// End Redis connection
 
 var router = express.Router();
 
